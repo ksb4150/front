d4ba49578b5a6cc684015a95b2a44c01ee48974f
김수빈 <38367572+Ksubin0@users.noreply.github.com>
2025-02-10 23:46:56 +0900

    test

--- src/ConnectCaller.tsx
+++ src/ConnectCaller.tsx
@@ -16,7 +16,7 @@ const ConnectCaller: React.FC = () => {
     setError(null);
 
     try {
-      const response = await axios.get(`http://back-container-test:5000/api/upbit/${endpoint}`);
+      const response = await axios.get(`http://localhost:3000/api/upbit/${endpoint}`);
       setData(response.data);
     } catch (err: any) {
       setError(err.message);
